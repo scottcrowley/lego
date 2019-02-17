b4c69f14d308c2a0f2d744aec65bf005223cb392
Scott Crowley <scott@scottcrowley.com>
2019-02-17 11:42:30 -0700

    Update tests

--- tests/Feature/ProfilesTest.php
+++ tests/Feature/ProfilesTest.php
@@ -29,7 +29,6 @@ class ProfilesTest extends TestCase
     /** @test */
     public function a_user_may_view_edit_page_for_their_own_profile()
     {
-        $this->withoutExceptionHandling();
         $this->signIn($user = create('App\User'));
 
         $this->get(route('profiles.edit'))
@@ -40,7 +39,6 @@ class ProfilesTest extends TestCase
     /** @test */
     public function a_user_may_update_their_profile()
     {
-        $this->withoutExceptionHandling();
         $this->signIn($user = create('App\User'));
 
         $user->name = 'New Name';
