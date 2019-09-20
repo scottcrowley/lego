815340655137ee62ca5b95d79ba6e63d3ec39db0
= <=>
2019-09-20 11:43:11 -0700

    Change card formatting on smaller screens

--- resources/views/lego/inventory_parts/grid.blade.php
+++ resources/views/lego/inventory_parts/grid.blade.php
@@ -2,18 +2,18 @@
 
 @section('content')
 <div class="w-full px-3 sm:px-0">
-    <div class="flex bg-white border mb-8 md:mx-auto md:p-6 md:w-2/3 p-3 rounded-lg shadow-md w-full" style="width: 95%;">
+    <div class="flex flex-col lg:flex-row bg-white border mb-8 mx-auto md:p-6 md:w-2/3 p-3 rounded-lg shadow-md w-full" style="width: 95%;">
         <div class="w-auto max-w-md">
-            <img src="{{ $inventory->set->image_url }}" alt="{{ $inventory->set->name }}">
+            <img src="{{ $inventory->set->image_url }}" alt="{{ $inventory->set->name }}" class="block mx-auto lg:mx-0">
         </div>
-        <div class="flex-1 ml-5 pt-6">
+        <div class="flex-1 m-0 lg:ml-5 pt-6">
             <div class="flex flex-col h-full">
                 <div>
                     <p class="title text-md md:text-2xl mb-4">{{ $inventory->set->name }}</p>
-                    <p class="text-sm md:text-base mb-2 font-hairline tracking-wide"><span class="font-semibold">Set Number:</span> {{ $inventory->set->set_num }}</p>
-                    <p class="text-sm md:text-base mb-2 font-hairline tracking-wide"><span class="font-semibold">Theme:</span> {{ $inventory->set->theme->theme_label }}</p>
-                    <p class="text-sm md:text-base mb-2 font-hairline tracking-wide"><span class="font-semibold">Year:</span> {{ $inventory->set->year }}</p>
-                    <p class="text-sm md:text-base font-hairline tracking-wide"><span class="font-semibold">Pieces:</span> {{ $inventory->set->num_parts }}</p>
+                    <p class="text-sm md:text-base mb-2 font-normal tracking-wide"><span class="font-semibold">Set Number:</span> {{ $inventory->set->set_num }}</p>
+                    <p class="text-sm md:text-base mb-2 font-normal tracking-wide"><span class="font-semibold">Theme:</span> {{ $inventory->set->theme->theme_label }}</p>
+                    <p class="text-sm md:text-base mb-2 font-normal tracking-wide"><span class="font-semibold">Year:</span> {{ $inventory->set->year }}</p>
+                    <p class="text-sm md:text-base font-normal tracking-wide"><span class="font-semibold">Pieces:</span> {{ $inventory->set->num_parts }}</p>
                 </div>
                 <div class="mt-auto self-end">
                     <a href="{{ url()->previous() }}" class="btn is-primary px-3 py-1 text-xs">Go Back</a>
