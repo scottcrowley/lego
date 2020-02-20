076492187de2de6e94dac5b954771e8087f141a6
= <=>
2020-02-20 08:17:05 -0700

    Add additional test for unassigned parts

--- cypress/integration/storageUnassignedParts.spec.js
+++ cypress/integration/storageUnassignedParts.spec.js
@@ -15,6 +15,25 @@ describe('Storage - Unassigned Parts', () => {
         cy.get('.card-container .card').should('have.length', 6);
     });
 
+    it('can select parts to move', () => {
+        cy.visit('/storage/locations/parts/unassigned');
+
+        cy.get('.card-container .card:nth-child(4) .card-content [data-cy=select-part-button]')
+            .dblclick()
+            .parent('.card-content')
+            .should('not.have.class', 'border-primary');
+
+        cy.get('.card-container .card:nth-child(5) .card-content [data-cy=select-part-button]')
+            .click()
+            .parent('.card-content')
+            .should('have.class', 'border-primary');
+        
+        cy.get('.card-container .card:nth-child(6) .card-content [data-cy=select-part-button]')
+            .click();
+        
+        cy.get('.card-container .card .card-content.border-primary').should('have.length', 2);
+    });
+
     it('can select a storage location and move parts', () => {
         cy.visit('/storage/locations/parts/unassigned');
 
@@ -25,13 +44,10 @@ describe('Storage - Unassigned Parts', () => {
         cy.get('[data-cy=button-move-selected]').should('be.disabled');
         cy.get('[data-cy=button-move-all]').should('not.be.disabled');
 
-        cy.get('.card-container .card:nth-child(5) .card-content [data-cy=select-part-button]')
-            .click()
-            .parent('.card-content')
-            .should('have.class', 'border-primary');
+        cy.get('.card-container .card:nth-child(5) .card-content [data-cy=select-part-button]').click();
         cy.get('[data-cy=button-move-selected]').should('not.be.disabled');
-        cy.get('.card-container .card:nth-child(6) .card-content [data-cy=select-part-button]')
-            .click();
+        cy.get('.card-container .card:nth-child(6) .card-content [data-cy=select-part-button]').click();
+        
         cy.get('[data-cy=button-move-selected]').click();
         cy.get('.card-container .card').should('have.length', 4);
 
